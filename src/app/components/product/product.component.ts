fabf07331832136eaf3c0163079fbd8c8a6c24d0
vanchor <vovanyakorev2@gmail.com>
2023-02-26 16:12:54 +0100

    display all products in main page

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,8 +1,13 @@
-import {Component} from '@angular/core'
+import {Component, Input} from '@angular/core'
+import {IProduct} from "../../models/product";
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html'
 })
 
-export class ProductComponent {}
+export class ProductComponent {
+  @Input() product: IProduct
+
+  details: boolean = false
+}
