03cd2140067729fdf7c9efce42d3bab3ac0571aa
vanchor <vovanyakorev2@gmail.com>
2023-02-27 21:47:13 +0100

    added pop up modal for creating new product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,28 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from "./components/product/product.component";
 import { HttpClientModule } from "@angular/common/http";
 import { GlobalErrorComponent } from './components/global-error/global-error.component';
-import { FormsModule } from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { FilterProductsPipe } from './pipes/filter-products.pipe';
+import { ModalComponent } from './components/modal/modal.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
+import { FocusDirective } from './directives/focus.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     GlobalErrorComponent,
-    FilterProductsPipe
+    FilterProductsPipe,
+    ModalComponent,
+    CreateProductComponent,
+    FocusDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/create-product/create-product.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.css']
+})
+export class CreateProductComponent implements OnInit{
+  form = new FormGroup({
+    title: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6)
+    ])
+  })
+
+  get title(){
+    return this.form.controls.title as FormControl
+  }
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  submit(){
+    console.log(this.form.value)
+  }
+}
